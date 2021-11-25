07c28e95b7aec12f1ccbab171503ee450f5bdf29
AllenJoeG <allen.joeg@gmail.com>
2021-11-25 13:12:55 -0600

    flipToggle() and toggleImageDesc() implemented.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,15 +4,35 @@ import './GalleryItem.css';
 
 
 const GalleryItem = ({picture}) => {
+  let toggle = true;
+
+  const flipToggle = () => {
+    if (toggle) {
+      toggle = false;
+    } else {
+      toggle = true;
+    }
+  }
+
+  const toggleImageDesc = () => {
+    if (toggle) {
+      return <img
+              key={picture.id}
+              src={picture.path}
+            />
+    } else {
+      return <p>{picture.description}</p>
+    };
+  };
 
   return (
     <div>
+      <div className='imageDiv'>
+        {toggleImageDesc()};
+      </div>
       
-      <img
-        key={picture.id}
-        src={picture.path}
-      />
-        
+      <button>Love it!</button>
+      <p> {picture.likes} people love this!</p>
     </div>
   );
 };
